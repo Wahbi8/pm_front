3d437ac4ebcc15ddf5c19a7d94b7131056cebe47
Oussama_Wahbi <wahbi.oussama08@gmail.com>
2026-02-11 21:22:14 +0100

    -m

--- src/Apis.tsx
+++ src/Apis.tsx
@@ -1,3 +1,37 @@
-import React from "react";
+// import React from "react";
 import axios from "axios";
 
+interface HelloResponse { 
+    message: string;
+}
+
+const GetAllInvoices = async (): Promise<void> => {
+    try {
+        const res = await axios.get<HelloResponse>(
+            "http://localhost:5000/api/GetAllInvoices"
+        )
+        
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const CreateInvoice = async (): Promise<void> => {
+    try {
+        const res = await axios.post<HelloResponse>(
+            "http://localhost:5000/api/invoice/CreateInvoice"
+        )
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const UpdateInvoice = async (): Promise<void> => {
+    try {
+        const res = await axios.post<HelloResponse>(
+            "http://localhost:5000/api/invoice/UpdateInvoice"
+        )
+    } catch (err) {
+        console.error(err)
+    }
+}
